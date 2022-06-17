1af3d24f806cc78d5ef0fb26aeaf9dd5fb279308
Quinten Coret <Qcoret@gmail.com>
2022-06-17 21:11:42 +0200

    Writing some tests

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /**
  * @type {import('postcss').PluginCreator}
  */
- module.exports = (opts = {}) => {
+ module.exports = () => {
   // Work with options here
 
   return {
     postcssPlugin: 'postcss-short-vars',
 
-    Declaration (decl, postcss) {
+    Declaration (decl) {
       if (!decl || !decl.value.includes('--')) return;
 
       const arr = decl.value.split(' ').map((v) => {
         if (v.match(/var(.*)/)) return v;
-        if (v.match(/--.+/)) return `var(${v})`;
+        if (v.match(/--.+/)) return v.replace(/(--.+)/g, 'var($1)').replace(',)', '),');
         return v;
       });
 
--- index.test.js
+++ index.test.js
@@ -2,16 +2,32 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+async function run (input, output) {
+  let result = await postcss([plugin()]).process(input, { from: undefined })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
+it('handles simple custom prop', async () => {
+  await run('a{ color: --blue; }', 'a{ color: var(--blue); }')
+});
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
+it('handles custom prop with other value', async () => {
+  await run('a{ color: --blue #fff; }', 'a{ color: var(--blue) #fff; }')
+});
 
-*/
+it('handles multiple custom props', async () => {
+  await run('a{ color: --blue --red; }', 'a{ color: var(--blue) var(--red); }')
+});
+
+it('handles custom prop in function', async () => {
+  await run('a{ color: rgba(--blue); }', 'a{ color: rgba(var(--blue)); }')
+});
+
+it('handles multiple custom props in function', async () => {
+  await run('a{ color: rgba(--blue, --red); }', 'a{ color: rgba(var(--blue), var(--red)); }')
+});
+
+it('does not handle custom prop definition', async () => {
+  await run('a{ --blue: #fff; }', 'a{ --blue: #fff; }')
+});
